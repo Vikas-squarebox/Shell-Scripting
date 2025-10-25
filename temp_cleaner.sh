4dbef2525bf2146c66fec6acb3d30eef494940d6
vikas puri <vikaspuri422@gmail.com>
2025-10-25 19:39:31 +0530

    Temp file cleaner added

---
+++ temp_cleaner.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#clean temporary files older than a specified number of days
+
+DIR="/tmp"
+DAYS=7
+
+echo "Cleaning up files in $DIR older than $DAYS days..."
+find $DIR -type f -mtime +$DAYS -exec rm -f {} \;
+echo "Cleanup complete."
